d7cda49c5cbcfa6054f40a13a12b5a2ef3b9ce02
Adrian Enachi <enachi.adrian52@gmail.com>
2024-09-26 15:12:54 +0200

    added user login/register/jwt

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,9 @@ import Router from 'express';
 const router = Router();
 
 import User from '../Model/User.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
 
 
 // Get all users
@@ -14,6 +17,96 @@ router.get('/api/users', async (req, res) => {
         res.status(404).json({ error: error.message });
     }
 });
+
+
+// Register
+router.post("/api/register", async (req, res) => {
+    // Our register logic starts here
+   try {
+        // Get user input
+        const { username, password, email} = req.body;
+
+        // Validate user input
+        if (!(username && email && password )) {
+            return res.status(400).send("All input are required");
+        }
+
+        // checks if user already exist
+        // Validates if user exist in our database
+        const oldUser = await User.findOne({ username });
+
+        if (oldUser) {
+            return res.status(409).send("Username already in use");
+        } 
+
+        //Encrypt user password
+        const encryptedUserPassword = await bcrypt.hash(password, 10);
+
+        // Create user in our database
+        const user = await User.create({
+            username: username.toLowerCase(), // sanitize 
+            email: email.toLowerCase(), // sanitize
+            password: encryptedUserPassword,
+        });
+
+        // return new user
+        return res.status(201).json(user);
+
+    } catch (error) {
+        console.log({ error: error.message });
+    }
+});
+
+// Login
+router.post("/api/login", async (req, res) => {
+    try {
+        // Get user input
+        const { username, password } = req.body;
+    
+        // Validate user input
+        if (!(username && password)) {
+            res.status(400).send("All inputs are required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ username });
+        
+    
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign(
+                { userId: user._id, username: user.username, email: user.email },
+                process.env.SECRET_KEY,
+                { expiresIn: "5h" }
+            );
+
+            // create cookie
+            res.cookie('jwt-TravelDestination', token, {
+                httpOnly: true,
+                maxAge: 5 * 60 * 60 * 1000 // 5 hours
+                
+            })
+    
+            
+            return res.status(200).json(user);
+        }
+        return res.status(400).send("Invalid Credentials");
+    } catch (error) {
+        return res.status(400).json({ error: error.message })
+    }    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Create a new user
 router.post('/api/users', async (req, res) => {
