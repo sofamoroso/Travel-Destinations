3e8660a5b4359660ac04bfdf94cad02b9d8abca9
Enachi Adrian <enachi.adrian52@gmail.com>
2024-10-07 12:49:11 +0200

    toggle the visibility of the add/edit/delete destination button

--- client/js/authHandler.js
+++ client/js/authHandler.js
@@ -9,6 +9,12 @@ function initializeAuth() {
 
 	const loggedIn = !!token;
 
+	// Ensure that the selected user is the same as logged in user on page refresh
+	if (loggedIn && username) {
+		sessionStorage.setItem('selectedUser', username);
+		sessionStorage.setItem('selectedUserId', _id);
+	}
+
 	console.log(loggedIn, username, _id);
 
 	const loginModal = document.getElementById('modal');
@@ -123,8 +129,6 @@ const login = async (username, password) => {
 		const data = await response.json();
 
 		if (response.ok) {
-			loggedIn = true;
-
 			// Store JWT token in session storage
 			sessionStorage.setItem('jwt-TravelDestination', data.token);
 			sessionStorage.setItem('logged-username', data.username);
