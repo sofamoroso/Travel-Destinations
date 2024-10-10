b8a3abbc4a0fad2d9669611bd38879ab5ef6b84c
Adrian Enachi <enachi.adrian52@gmail.com>
2024-10-10 15:20:38 +0200

    vercel fix

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'client' directory
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, 'client')));
 
 // Your routes here
 app.use(travelRoutes);
